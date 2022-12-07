89f9885df4232a22162c5f44dd4b535e21c8a093
Harsh Kedia <harshkedia717@gmail.com>
2022-12-07 11:29:43 +0530

    changes to linking

--- js/index.js
+++ js/index.js
@@ -17,3 +17,7 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+document.querySelector(".campuscol").addEventListener("click", () => {
+  window.location.href = "all-courses.html";
+});
